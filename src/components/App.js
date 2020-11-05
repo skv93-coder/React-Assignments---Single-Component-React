37fd15892f58e42908abc598bf3557b659d80174
skv93-coder <68867418+skv93-coder@users.noreply.github.com>
2020-11-05 15:11:47 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <></>
-  )
-}
-
+    <>
+      <p> "I am learning React. My life is getting better. "</p>
+    </>
+  );
+};
 
 export default App;
